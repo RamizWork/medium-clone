d9357c8d2088b6079bdf3143c929c97d7964edb2
dzhabrailov <dzhabrailov@perco.ru>
2025-03-07 22:37:32 +0300

    create register and register request interfaces

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,8 +1,6 @@
 import {createAction, props} from '@ngrx/store';
 
 import {ActionTypesEnum} from '../enums/action-types..enum';
+import {RegisterRequestInterface} from '../../types/registerRequest.interface';
 
-export const registerAction = createAction(
-  ActionTypesEnum.REGISTER,
-  props<{userName: string; email: string; password: string}>()
-);
+export const registerAction = createAction(ActionTypesEnum.REGISTER, props<RegisterRequestInterface>());
