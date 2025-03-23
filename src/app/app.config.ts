63d28102c443e837786e3274eef0ed178adbe5e6
dzhabrailov <dzhabrailov@perco.ru>
2025-03-23 22:40:41 +0300

    an effects library was added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import {provideStore, StoreRootModule} from '@ngrx/store';
 import {provideStoreDevtools} from '@ngrx/store-devtools';
+import {provideEffects} from '@ngrx/effects';
 
 import {routes} from './app.routes';
 
@@ -17,5 +18,6 @@ export const appConfig: ApplicationConfig = {
       maxAge: 25, // Retains last 25 states
       logOnly: !isDevMode(), // Restrict extension to log-only mode
     }),
+    provideEffects(),
   ],
 };
